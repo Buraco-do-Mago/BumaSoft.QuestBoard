578f5970421e88bb694aa7b7df8eb1022508d24e
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-03-15 21:53:52 -0300

    Improving the dependency injection logic so it injects the correct bus type

--- src/BumaSoft.QuestBoard/DependencyInjection/QuestBoardDependencyInjection.cs
+++ src/BumaSoft.QuestBoard/DependencyInjection/QuestBoardDependencyInjection.cs
@@ -1,3 +1,4 @@
+using BumaSoft.QuestBoard.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BumaSoft.QuestBoard.DependencyInjection;
@@ -6,9 +7,12 @@ public static class QuestBoardDependencyInjection
 {
     public static IServiceCollection AddQuestBoard(this IServiceCollection services, Action<QuestBoardConfiguration>? configure = null)
     {
-        services.AddScoped<Bus>();
+        var configuration = new QuestBoardConfiguration(services);
         if (configure is not null)
-            configure(new QuestBoardConfiguration(services));
+            configure(configuration);
+        var busType = configuration.BusType;
+        services.AddScoped(busType);
+        services.AddScoped(typeof(IBus), serviceProvider => serviceProvider.GetRequiredService(busType));
         return services;
     }
 }
